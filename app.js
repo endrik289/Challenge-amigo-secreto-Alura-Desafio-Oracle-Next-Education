4dd9f80497854ed8ded4a7663b41f4c10b860a44
endrik289 <endrik289@gmail.com>
2025-09-10 17:18:14 -0300

    Função para Adicionar Nome javascript

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ let amigos = [];
 let nomeInput = document.getElementById('amigo');
 let listaAmigos = document.getElementById('listaAmigos'); 
 let resultado = document.getElementById('resultado');
+
+function adicionarAmigo() {
+    let nome = nomeInput.value.trim();
+    if (!nome) {
+        alert('Por favor, digite um nome válido!');
+        return;
+    }
+    amigos.push(nome);
+    atualizarLista();
+    nomeInput.value = '';
+    nomeInput.focus();
+}
